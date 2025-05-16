e5bd93733071c85a11012656333ccf6a5d516255
AjayU8 <Ajayajurfc@gmail.com>
2025-05-16 14:50:07 +0530

    intial commit

--- git1.py
+++ git1.py
@@ -1,1 +1,10 @@
-git
+class School:
+    def __init__(self,student_name):
+        self.student_name =student_name 
+    def show_student(self):
+         print("student name is:",self.student_name)
+                        
+s1 =School("arun")
+s1.show_student()
+
+
